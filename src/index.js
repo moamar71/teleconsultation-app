c00e6d4addfc3ca7d428e7b172beb3eb2ce873a1
mh <121820449+moamar71@users.noreply.github.com>
2025-03-25 05:15:24 +0100

    Remove duplicate BrowserRouter from index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // استيراد BrowserRouter
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* لف App داخل BrowserRouter */}
-      <App />
-      <ToastContainer />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
